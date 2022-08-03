de551380f1c06daffe2d6a0cd6f80d659f96bf3e
Chris Kurecka <ckurecka@gmail.com>
2022-08-03 00:14:44 -0400

    browser router tweaks 6

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter  basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <App />
-    </HashRouter >
+    </HashRouter>
   </React.StrictMode>
 );
 
@@ -18,3 +18,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//
+//
